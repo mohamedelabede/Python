61cbcc3134f7f81efc00bdf6545cd92880587c41
mohamedelabede <mohamed.ali@meti.dev>
2024-12-30 16:44:38 +0200

    Second Commit

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-start_year = input("Enter the first year : ")
-end_year = input("Enter the end year : ")
-age = int(end_year) - int(start_year)
-print("Age :", age)
+first_number = int(input("First Num : "))
+second_number = int(input("Second Num : "))
+sum = first_number + second_number
+subtraction = first_number - second_number
+multiplication =  first_number * second_number
+if second_number != 0:
+    division = first_number / second_number
+else:
+    division = "Error: Division by zero"
+
+print("Sum =",sum ,"\n Sub =",subtraction, "\n Multi =",multiplication, "\n Div =",division )
