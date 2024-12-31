d692214518a1a75b9dddf104107d679877d9936e
mohamedelabede <mohamed.ali@meti.dev>
2024-12-31 16:50:13 +0200

    Update Merge_Sort.py

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -1,23 +1,18 @@
 def merge(arr, left, mid, right):
     n1 = mid - left + 1
     n2 = right - mid
-
-    # Create temp arrays
     L = [0] * n1
     R = [0] * n2
 
-    # Copy data to temp arrays L[] and R[]
     for i in range(n1):
         L[i] = arr[left + i]
     for j in range(n2):
         R[j] = arr[mid + 1 + j]
 
-    i = 0  # Initial index of first subarray
-    j = 0  # Initial index of second subarray
-    k = left  # Initial index of merged subarray
-
-    # Merge the temp arrays back
-    # into arr[left..right]
+    i = 0  
+    j = 0  
+    k = left  
+   
     while i < n1 and j < n2:
         if L[i] <= R[j]:
             arr[k] = L[i]
@@ -27,15 +22,12 @@ def merge(arr, left, mid, right):
             j += 1
         k += 1
 
-    # Copy the remaining elements of L[],
-    # if there are any
     while i < n1:
         arr[k] = L[i]
         i += 1
         k += 1
 
-    # Copy the remaining elements of R[], 
-    # if there are any
+   
     while j < n2:
         arr[k] = R[j]
         j += 1
@@ -54,7 +46,6 @@ def print_list(arr):
         print(i, end=" ")
     print()
 
-# Driver code
 if __name__ == "__main__":
     arr = list(input("Enter the array elements: "))
 
